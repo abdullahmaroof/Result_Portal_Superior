e2892655c8cd10ecb74af8405d6c61a5db2a6add
abdullahmaroof <abdullahmaroof597@gmail.com>
2022-04-13 20:50:47 -0700

    Exam Portal Project Python

--- Create_DB_1stsemester.py
+++ Create_DB_1stsemester.py
@@ -10,18 +10,18 @@ semesterone_data = pd2[['Roll no','Semester','Calculus','English','Introduction 
 print(semesterone_data)
 #i=0
 #while i<=173:
-# try:
-#     con = connect("studentrecord.db")
-#     cursor = con.cursor()
-#     #sql = """CREATE TABLE std_semone (roll_no TEXT, Semester TEXT, Calculus NUMBER, English NUMBER, ITC NUMBER,
-#     #PF NUMBER, AP NUMBER)"""
-#         #sql="INSERT INTO std_semone VALUES ('"+semesterone_data['Roll no'][i]+"','"+semesterone_data['Semester'][i]+"','"+str(semesterone_data['Calculus'][i])+"','"+str(semesterone_data['English'][i])+"','"+str(semesterone_data['Introduction To Computing'][i])+"','"+str(semesterone_data['Programming Fundamental '][i])+"','"+str(semesterone_data['Applied Physics'][i])+"')"
-#     sql = """SELECT * FROM std_semone"""
-#     cursor.execute(sql)
-#     x = cursor.fetchall()
-#     con.commit()
-#     con.close()
-#     print(x)
-# except Exception as error:
-#     print(error)
-#     #i+=1
+try:
+    con = connect("studentrecord.db")
+    cursor = con.cursor()
+    #sql = """CREATE TABLE std_semone (roll_no TEXT, Semester TEXT, Calculus NUMBER, English NUMBER, ITC NUMBER,
+     #           PF NUMBER, AP NUMBER)"""
+    #sql="INSERT INTO std_semone VALUES ('"+semesterone_data['Roll no'][i]+"','"+semesterone_data['Semester'][i]+"','"+str(semesterone_data['Calculus'][i])+"','"+str(semesterone_data['English'][i])+"','"+str(semesterone_data['Introduction To Computing'][i])+"','"+str(semesterone_data['Programming Fundamental '][i])+"','"+str(semesterone_data['Applied Physics'][i])+"')"
+    sql = """SELECT * FROM std_semone"""
+    cursor.execute(sql)
+    x = cursor.fetchall()
+    con.commit()
+    con.close()
+    print(x)
+except Exception as error:
+    print(error)
+    #i+=1
